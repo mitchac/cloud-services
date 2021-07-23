306a40be4e3c41af80d362fb32068154ea7a3927
Mitchell Cunningham <mitchac@gmail.com>
2021-07-23 14:10:13 +1000

    remove depends on from registry.tf

--- terraform/registry.tf
+++ terraform/registry.tf
@@ -3,5 +3,4 @@ resource "google_artifact_registry_repository" "cloud-services-repository" {
     location = var.region
     repository_id = "cloud-services-repository"
     format = "DOCKER"
-    depends_on = [google_project_service.registry-gcp-service]
 }
