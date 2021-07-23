ff09147cfa61b13770bb4d9a4ccba6d2dbc22942
Mitchell Cunningham <mitchac@gmail.com>
2021-07-23 15:35:09 +1000

    add subscription to pubsub topic in tf

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -2,3 +2,18 @@ resource "google_pubsub_topic" "helloworld" {
   name = var.pubsub_topic_name
   project = var.project
 }
+
+resource "google_pubsub_subscription" "helloworld" {
+  name  = "helloworld"
+  topic = google_pubsub_topic.helloworld.name
+
+  ack_deadline_seconds = 20
+
+  push_config {
+    push_endpoint = "https://cloudrun-srv-kvsfql577a-uc.a.run.app"
+
+    attributes = {
+      x-goog-version = "v1"
+    }
+  }
+}
