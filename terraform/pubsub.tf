f564d8b350a0abeabaf7d45584f45b16ae1047d3
Mitchell Cunningham <mitchac@gmail.com>
2021-07-24 10:22:56 +1000

    fix cloudrun url in pubsub subscription

--- terraform/pubsub.tf
+++ terraform/pubsub.tf
@@ -10,7 +10,7 @@ resource "google_pubsub_subscription" "helloworld" {
   ack_deadline_seconds = 20
 
   push_config {
-    push_endpoint = "https://cloudrun-srv-kvsfql577a-uc.a.run.app"
+    push_endpoint = google_cloud_run_service.cloudrun-srv.status[0].url
     oidc_token {
       service_account_email = google_service_account.pubsub-sa.email
     }
