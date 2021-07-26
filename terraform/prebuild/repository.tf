d1927a3d3091c826cc7c3450cd137674a172147e
Mitchell Cunningham <mitchac@gmail.com>
2021-07-26 15:00:38 +1000

    registry to repository

--- terraform/prebuild/repository.tf
+++ terraform/prebuild/repository.tf
@@ -1,7 +1,7 @@
-resource "google_artifact_registry_repository" "helloworld" {
+resource "google_artifact_registry_repository" "helloworld-ar" {
     provider = google-beta
     location = var.region
-    repository_id = var.registry
+    repository_id = var.repository
     format = "DOCKER"
     depends_on = [
       google_project_service.registry-gcp-service,
--- terraform/prebuild/variables.tf
+++ terraform/prebuild/variables.tf
@@ -3,5 +3,5 @@ variable "region" {
 variable "project" {
     default = "maximal-dynamo-308105"
 }
-variable "registry" {
+variable "repository" {
 }
