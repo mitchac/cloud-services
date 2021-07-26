dcefc99a85613ae6618ad06be41d3c211c8307c7
Mitchell Cunningham <mitchac@gmail.com>
2021-07-26 16:22:56 +1000

    recreate resources stop service removal on delete

--- terraform/postbuild/api.tf
+++ terraform/postbuild/api.tf
@@ -1,10 +1,10 @@
 resource "google_project_service" "cloudrun-gcp-service" {
   service = "run.googleapis.com"
   project = var.project
-  disable_on_destroy = true
+  disable_on_destroy = false
 }
 resource "google_project_service" "pubsub-gcp-service" {
   service = "pubsub.googleapis.com"
   project = var.project
-  disable_on_destroy = true
+  disable_on_destroy = false
 }
--- terraform/prebuild/api.tf
+++ terraform/prebuild/api.tf
@@ -1,5 +1,5 @@
 resource "google_project_service" "registry-gcp-service" {
   service = "artifactregistry.googleapis.com"
   project = var.project
-  disable_on_destroy = true
+  disable_on_destroy = false
 }
