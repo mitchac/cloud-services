25fd6204589b4a425e33409ea9cb25be027f92bd
Mitchell Cunningham <mitchac@gmail.com>
2021-07-26 14:48:11 +1000

    repository depends api

--- terraform/prebuild/registry.tf
+++ terraform/prebuild/registry.tf
@@ -1,6 +1,9 @@
-resource "google_artifact_registry_repository" "helloworld_repository" {
+resource "google_artifact_registry_repository" "helloworld" {
     provider = google-beta
     location = var.region
     repository_id = var.registry
     format = "DOCKER"
+    depends_on = [
+      google_project_service.registry-gcp-service,
+    ]
 }
