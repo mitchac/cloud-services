e8cb7ff2fdd478208b82d711093ae1e57a4598d1
Mitchell Cunningham <mitchac@gmail.com>
2021-08-25 14:59:34 +1000

    ditto

--- cloudrun/terra-api-test/main.py
+++ cloudrun/terra-api-test/main.py
@@ -15,14 +15,14 @@ def index():
     terra_api = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(terra_api)
 
-    token = os.popen('curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google"')
+    token = os.popen('curl -s "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" | tac | tac')
 
 
     #token = requests.get('http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token')
     
     #workflow_config_response = terra_api.get_workflow_config("firstterrabillingaccount", "singlem-pilot-2", "singlem", "singlem-single-task")
 
-    pprint(token)
+    print(token)
 
     return ("", 204)
 
