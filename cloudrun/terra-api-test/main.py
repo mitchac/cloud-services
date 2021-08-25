361ba19af97c0e995e929680c113df81099a5165
Mitchell Cunningham <mitchac@gmail.com>
2021-08-25 11:33:23 +1000

    fix cloudrun main.py

--- cloudrun/terra-api-test/main.py
+++ cloudrun/terra-api-test/main.py
@@ -1,23 +1,18 @@
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
 from pprint import pprint
-import os
-import json
-import sys
 
 from flask import Flask, request
 
 import importlib.util
-spec = importlib.util.spec_from_file_location("terra_api", "terra_api.py")
-terra_api = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(terra_api)
-
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
 def index():
     
+    spec = importlib.util.spec_from_file_location("terra_api", "terra_api.py")
+    terra_api = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(terra_api)
+
     workflow_config_response = terra_api.get_workflow_config("firstterrabillingaccount", "singlem-pilot-2", "singlem", "singlem-single-task")
 
     pprint(workflow_config_response)
